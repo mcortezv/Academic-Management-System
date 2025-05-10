2a1b8f3e7f87cab7eb956aad4ade25df367af636
mcortezv <mdjesuscv@gmail.com>
2025-05-09 22:20:30 -0700

    Project Structure

---
+++ StudentManagementSystem/src/main/java/StudentManagementSystem.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author mdjes
+ */
+public class StudentManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
