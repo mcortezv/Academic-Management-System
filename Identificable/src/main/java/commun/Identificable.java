206b3c3448ea0bf1f18fef1d812bf507b0e263ef
mcortezv <mdjesuscv@gmail.com>
2025-05-10 21:02:33 -0700

    Module Identificable

---
+++ Identificable/src/main/java/commun/Identificable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package commun;
+
+/**
+ *
+ * @author mdjes
+ */
+public interface Identificable extends Comparable<Identificable> {
+    String getId();
+    
+    Double getAverage();
+}
